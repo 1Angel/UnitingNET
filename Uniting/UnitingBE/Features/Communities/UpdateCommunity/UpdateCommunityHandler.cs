4caf242fc63542502fd80996872de7101516c559
Angel Abreu <iangeeel01@gmail.com>
2025-02-25 23:20:45 -0400

    somes changes

--- Uniting/UnitingBE/Features/Communities/UpdateCommunity/UpdateCommunityHandler.cs
+++ Uniting/UnitingBE/Features/Communities/UpdateCommunity/UpdateCommunityHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
+using UnitingBE.Common;
 using UnitingBE.Database;
 
 namespace UnitingBE.Features.Communities.UpdateCommunity
@@ -8,19 +9,23 @@ namespace UnitingBE.Features.Communities.UpdateCommunity
     public class UpdateCommunityHandler : IRequestHandler<UpdateCommunityRequest, IResult>
     {
         private readonly AppDBContext _context;
-        public UpdateCommunityHandler(AppDBContext context)
+        private readonly CurrentUser _currentUser;
+        public UpdateCommunityHandler(AppDBContext context, CurrentUser currentUser)
         {
             _context = context;
+            _currentUser = currentUser;
         }
         public async Task<IResult> Handle(UpdateCommunityRequest request, CancellationToken cancellationToken)
         {
             var communityId = await _context.communities.Where(x => x.Id == request.communityId).FirstOrDefaultAsync();
-            if(communityId != null)
+            if(communityId.AppUserId == _currentUser.GetUserId())
             {
-                communityId.Name = request.Name;
-                communityId.Description = request.Description;
+                return Results.Forbid();
             }
-
+    
+            communityId.Name = request.Name;
+            communityId.Description = request.Description;
+            
             var result = await _context.SaveChangesAsync();
             return Results.Ok();
         }
