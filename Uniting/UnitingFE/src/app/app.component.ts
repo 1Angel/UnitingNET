f91e92a4ec5c5d04b1925ddf51ab4493f855345a
Angel Abreu <iangeeel01@gmail.com>
2025-03-03 23:08:18 -0400

    update - frontend

--- Uniting/UnitingFE/src/app/app.component.ts
+++ Uniting/UnitingFE/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'UnitingFE';
