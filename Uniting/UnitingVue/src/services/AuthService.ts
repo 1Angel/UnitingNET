2550d5326f3b4cf28e622f4850b0cd6e98bf8dc6
Angel Abreu <iangeeel01@gmail.com>
2025-03-23 21:56:04 -0400

    frontend-register added

--- Uniting/UnitingVue/src/services/AuthService.ts
+++ Uniting/UnitingVue/src/services/AuthService.ts
@@ -8,4 +8,10 @@ export const AuthService={
         return httpClient.post<AuthResponse>('/auth/login', {email, password});
     }
 
+export const RegisterUser= async(username: string, email: string, password: string)=>{
+    return httpClient.post<AuthResponse>('/auth/register', {username: username, email: email, password: password});
+}
+
+export const GetCurrentUser = async()=>{
+    return httpClient.get<User>('/auth/current-user');
 }
