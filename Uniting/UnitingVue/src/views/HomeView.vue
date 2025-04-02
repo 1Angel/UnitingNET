74eb4f2d4b2fd31280bea39de12002f51e6b012f
Angel Abreu <iangeeel01@gmail.com>
2025-04-01 23:48:57 -0400

    Update HomeView.vue

--- Uniting/UnitingVue/src/views/HomeView.vue
+++ Uniting/UnitingVue/src/views/HomeView.vue
@@ -29,8 +29,8 @@ async function getUserFeed() {
     <div class="flex w-full max-w-6xl">
 
       <div class="w-2/3 space-y-6" v-if="!isLoggedIn">
-        <div class="bg-gray-800 border-1 rounded-2xl py-10">
-          <p class="ml-10 pt-20 text-white font-medium text-3xl">Log In to discover all the communities 🎇🎇🎇</p>
+        <div class="bg-gray-800 border-1 rounded-2xl py-22">
+          <p class="ml-10 text-white font-medium text-3xl">Log In to discover all the communities 🎇🎇🎇</p>
         </div>
       </div>
       <!-- Contenido principal: Lista de elementos -->
